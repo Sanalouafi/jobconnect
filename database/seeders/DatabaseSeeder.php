8fa26477c64cc3344b1ce73ac704cce120684796
Sanalouafi <sanalouafi2003@gmail.com>
2024-02-27 19:54:09 +0100

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,9 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        // $this->call([
-        //     RoleSeeder::class,
-        // ]);
+        $this->call([
+            RoleSeeder::class,
+        ]);
 
         // \App\Models\Company::factory(5)->create();
         \App\Models\Experience::factory(5)->create();
